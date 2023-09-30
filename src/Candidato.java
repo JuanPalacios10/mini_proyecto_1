514e9987408bb2a31f9c079a19c06afb350ef7fe
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2023-09-30 15:58:29 -0500

    Separacion del CRUD en un archivo separado

--- src/Candidato.java
+++ src/Candidato.java
@@ -3,10 +3,11 @@ public class Candidato extends Ciudadano {
     private String promesas[];
     private int votos;
 
-    public Candidato(String nombre, String cedula, boolean partido, String[] promesas) {
+    public Candidato(String nombre, String cedula, boolean partido, String[] promesas, int votos) {
         super(nombre, cedula);
         this.partido = partido;
         this.promesas = promesas;
+        this.votos = votos;
     }
     public boolean isPartido() {
         return partido;
@@ -19,5 +20,11 @@ public class Candidato extends Ciudadano {
     }
     public void setPromesas(String[] promesas) {
         this.promesas = promesas;
+    }
+    public int getVotos() {
+        return votos;
+    }
+    public void setVotos(int votos) {
+        this.votos = votos;
     }
 }
