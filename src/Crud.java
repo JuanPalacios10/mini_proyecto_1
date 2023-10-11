d170a482c6c9dc38b07d1cc3a670a5ef7b7bed25
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2023-10-11 17:31:19 -0500

    Creacion de funciones para organizar el partido con mas candidatos y el top 3 de las ciudades

--- src/Crud.java
+++ src/Crud.java
@@ -276,4 +276,109 @@ public abstract class Crud {
             System.out.println("- " + promesa);
         }
     }
+
+    public static void encontrarPartido(ArrayList<Candidato> candidatos) {
+        ArrayList<Short> numPartidos = new ArrayList<Short>();
+        ArrayList<Partidos> partidoCandidatos = new ArrayList<Partidos>();
+        Partidos partidosRepetidos[] = new Partidos[candidatos.size()];
+        short contadorPartidos = 0;
+
+        for(Candidato candidato : candidatos) {
+            partidosRepetidos[candidatos.indexOf(candidato)] = candidato.getPartido();
+        }
+        
+        for(Partidos partido : Partidos.values()) {
+            contadorPartidos = 0;
+
+            for(Partidos partidoRepetido : partidosRepetidos) {
+                if(partido.equals(partidoRepetido)) contadorPartidos++;
+            }
+
+            if(contadorPartidos > 0) {
+                numPartidos.add(contadorPartidos);
+                partidoCandidatos.add(partido);
+            }
+        }
+
+        short maxPartido = numPartidos.get(0);
+        int indicePartido = 0;
+
+        for(short i = 0; i < numPartidos.size(); i++) {
+            if(maxPartido < numPartidos.get(i)) {
+                maxPartido = numPartidos.get(i);
+                indicePartido = numPartidos.indexOf(numPartidos.get(i));
+            }
+        }
+
+        System.out.println("\nEl partido con mas candidatos es " + partidoCandidatos.get(indicePartido).toString().replace("_", " "));
+    }
+
+    public static void encontrarTopCiudades(ArrayList<Candidato> candidatos) {
+        ArrayList<Short> numCiudades = new ArrayList<Short>();
+        ArrayList<Ciudades> ciudadCandidatos = new ArrayList<Ciudades>();
+        Ciudades ciudadRepetidas[] = new Ciudades[candidatos.size()];
+        short contadorCiudades = 0;
+
+        for(Candidato candidato : candidatos) {
+            ciudadRepetidas[candidatos.indexOf(candidato)] = candidato.getCiudad();
+        }
+
+        for(Ciudades ciudad : Ciudades.values()) {
+            contadorCiudades = 0;
+
+            for(Ciudades ciudadRepetida : ciudadRepetidas) {
+                if(ciudad.equals(ciudadRepetida)) contadorCiudades++;
+            }
+
+            if(contadorCiudades > 0) {
+                numCiudades.add(contadorCiudades);
+                ciudadCandidatos.add(ciudad);
+            }
+        }
+
+        short menorNumCiudades[] = new short[3];
+        String topCiudades[] = new String[3];
+        int indiceCiudades;
+
+        if(numCiudades.size() == 1) {
+            topCiudades[0] = ciudadCandidatos.get(0).toString();
+            topCiudades[1] = "";
+            topCiudades[2] = "";
+        } else if(numCiudades.size() == 2) {
+            menorNumCiudades[0] = numCiudades.get(0);
+            
+            if(menorNumCiudades[0] > numCiudades.get(1)) {
+                topCiudades[0] = ciudadCandidatos.get(1).toString();
+                topCiudades[1] = ciudadCandidatos.get(0).toString();
+            } else {
+                topCiudades[0] = ciudadCandidatos.get(0).toString();
+                topCiudades[1] = ciudadCandidatos.get(1).toString();
+            }
+
+            topCiudades[2] = "";
+        } else {
+            for(short i = 0; i < menorNumCiudades.length; i++) {
+                indiceCiudades = 0;
+                menorNumCiudades[i] = numCiudades.get(0);
+
+                for(short j = 0; j < numCiudades.size(); j++) {
+                    if(menorNumCiudades[i] > numCiudades.get(j)) {
+                        menorNumCiudades[i] = numCiudades.get(j);
+                        indiceCiudades = numCiudades.indexOf(numCiudades.get(j));
+                    }
+                }
+
+                topCiudades[i] = ciudadCandidatos.get(indiceCiudades).toString();
+                if(numCiudades.size() > 1 && ciudadCandidatos.size() > 1) {
+                    numCiudades.remove(indiceCiudades);
+                    ciudadCandidatos.remove(indiceCiudades);
+                }
+            }
+        }
+
+        System.out.println("\nTop 3 de las ciudades con menos candidatos como ciudad de origen\n");
+        for(short i = 0; i < topCiudades.length; i++) {
+            System.out.println((i + 1) + ". " + topCiudades[i].replace("_", " "));
+        }
+    }
 }
